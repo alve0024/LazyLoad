064ef32a455707699ab89ef7526e901d8e4e4d81
Gerald Hurdle <hurdleg@algonquincollege.com>
2016-11-07 14:09:24 -0500

    Initial commit

---
+++ README.md
@@ -0,0 +1,18 @@
+## Lazy Load ##
+  Loading images in response to list requests
+
+#### Summary of Steps ####
+  Read the *TODO* comments to see the changes (View > Tool Windows... > TODO)
+
+  1. Edit **PlanetAdapter**
+     * display the planet's image
+
+## Source Code ##
+
+  Available from GitHub:
+
+  https://github.com/hurdleg/LazyLoad.git
+
+## Reference ##
+
+  Based on LazyLoad in _Connecting Android Apps to RESTful Web Services_ with David Gassner
---
+++ app/src/main/java/com/algonquincollege/hurdleg/planets/PlanetAdapter.java
@@ -0,0 +1,114 @@
+package com.algonquincollege.hurdleg.planets;
+
+import java.io.InputStream;
+import java.net.URL;
+import java.util.List;
+
+import android.app.Activity;
+import android.content.Context;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
+import android.os.AsyncTask;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ArrayAdapter;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+import com.algonquincollege.hurdleg.planets.model.Planet;
+
+/**
+ * Purpose: customize the Planet cell for each planet displayed in the ListActivity (i.e. MainActivity).
+ * Usage:
+ *   1) extend from class ArrayAdapter<YourModelClass>
+ *   2) @override getView( ) :: decorate the list cell
+ *
+ * Based on the Adapter OO Design Pattern.
+ *
+ * @author Gerald.Hurdle@AlgonquinCollege.com
+ *
+ * Reference: based on LazyLoad in "Connecting Android Apps to RESTful Web Services" with David Gassner
+ */
+public class PlanetAdapter extends ArrayAdapter<Planet> {
+
+    private Context context;
+    private List<Planet> planetList;
+
+    public PlanetAdapter(Context context, int resource, List<Planet> objects) {
+        super(context, resource, objects);
+        this.context = context;
+        this.planetList = objects;
+    }
+
+    @Override
+    public View getView(int position, View convertView, ViewGroup parent) {
+
+        LayoutInflater inflater =
+                (LayoutInflater) context.getSystemService(Activity.LAYOUT_INFLATER_SERVICE);
+        View view = inflater.inflate(R.layout.item_planet, parent, false);
+
+        //Display planet name in the TextView widget
+        Planet planet = planetList.get(position);
+        TextView tv = (TextView) view.findViewById(R.id.textView1);
+        tv.setText(planet.getName());
+
+        // TODO: Display planet photo in ImageView widget
+        // Lazy initialize a planet's bitmap image
+        System.out.print("planet: " + planet.getName());
+        if (planet.getBitmap() != null) {
+            System.out.println("\tbitmap in memory");
+            ImageView image = (ImageView) view.findViewById(R.id.imageView1);
+            image.setImageBitmap(planet.getBitmap());
+        }
+        else {
+            System.out.println("\tfetching bitmap using AsyncTask");
+            PlanetAndView container = new PlanetAndView();
+            container.planet = planet;
+            container.view = view;
+
+            ImageLoader loader = new ImageLoader();
+            loader.execute(container);
+        }
+
+        return view;
+    }
+
+    // container for AsyncTask params
+    private class PlanetAndView {
+        public Planet planet;
+        public View view;
+        public Bitmap bitmap;
+    }
+
+    private class ImageLoader extends AsyncTask<PlanetAndView, Void, PlanetAndView> {
+
+        @Override
+        protected PlanetAndView doInBackground(PlanetAndView... params) {
+
+            PlanetAndView container = params[0];
+            Planet planet = container.planet;
+
+            try {
+                String imageUrl = MainActivity.IMAGES_BASE_URL + planet.getImage();
+                InputStream in = (InputStream) new URL(imageUrl).getContent();
+                Bitmap bitmap = BitmapFactory.decodeStream(in);
+                planet.setBitmap(bitmap);
+                in.close();
+                container.bitmap = bitmap;
+                return container;
+            } catch (Exception e) {
+                e.printStackTrace();
+            }
+
+            return null;
+        }
+
+        @Override
+        protected void onPostExecute(PlanetAndView result) {
+            ImageView image = (ImageView) result.view.findViewById(R.id.imageView1);
+            image.setImageBitmap(result.bitmap);
+            result.planet.setBitmap(result.bitmap);
+        }
+    }
+}
